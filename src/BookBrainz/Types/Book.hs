59e732bb39f3b15270aa295566c80227b5f4ff46
Oliver Charles <oliver.g.charles@googlemail.com>
2011-07-11 17:43:55 +0100

    Silence some import warnings

--- src/BookBrainz/Types/Book.hs
+++ src/BookBrainz/Types/Book.hs
@@ -4,10 +4,8 @@ module BookBrainz.Types.Book
 
 import BookBrainz.Types.AuthorCredit
 import BookBrainz.Types.Newtypes (BookId)
-import Data.Maybe (fromJust)
 import Data.Text (Text)
-import Data.ByteString.Char8 (unpack)
-import Data.UUID (UUID, fromString)
+import Data.UUID (UUID)
 
 data Book = Book
             { bookName :: Text
