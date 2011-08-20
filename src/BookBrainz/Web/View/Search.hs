52aa5da5044aa1c8b49ef6bc3281d111219c9674
Oliver Charles <oliver.g.charles@googlemail.com>
2011-08-20 01:07:17 +0100

    Show roles in book search results

--- src/BookBrainz/Web/View/Search.hs
+++ src/BookBrainz/Web/View/Search.hs
@@ -1,13 +1,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 module BookBrainz.Web.View.Search where
 
-import Search.ElasticSearch (Document, SearchResults, getResults)
-import Text.Blaze          (Html)
+import           Data.Monoid          (mconcat)
+
+import           Data.Copointed       (copoint)
+import           Search.ElasticSearch (SearchResults(..)
+                                      ,SearchResult(..))
+import           Text.Blaze           (Html, toHtml)
+import qualified Text.Blaze.Html5 as H
 
 import qualified BookBrainz.Search as S
-import BookBrainz.Web.View (pageLayout, detailTable, linkBook)
+import           BookBrainz.Types
+import           BookBrainz.Web.View  (pageLayout, detailTable, linkBook
+                                      ,linkPerson)
 
 searchResults :: SearchResults S.SearchableBook -> Html
 searchResults results = pageLayout Nothing $
-  detailTable [("Book", [])] $ formatResult `map` getResults results
-  where formatResult r = [ linkBook $ S.bookResult r ]
+  detailTable [("Score", ["score"])
+              ,("Book", [])
+              ,("People", [])
+              ]
+            $ formatResult `map` getResults results
+  where formatResult r = [ toHtml $ score r
+                         , linkBook $ S.bookResult $ result r
+                         , roleList $ S.bookRoles $ result r
+                         ]
+
+roleList :: [(LoadedEntity Role, LoadedCoreEntity Person)]
+         -> Html
+roleList roles = H.ul (formatRole `mapM_` roles)
+  where formatRole (r, p) = H.li $ do
+          linkPerson p
+          mconcat [" (", toHtml (roleName (copoint r)), ")"]
