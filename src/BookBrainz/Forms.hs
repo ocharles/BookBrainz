1c8fb45c57afac6a4a7923f805fe152a9f7afa78
Oliver Charles <oliver.g.charles@googlemail.com>
2011-08-25 00:56:32 +0100

    Make sure /search works too

--- src/BookBrainz/Forms.hs
+++ src/BookBrainz/Forms.hs
@@ -1,29 +1,33 @@
 {-# LANGUAGE OverloadedStrings, TypeSynonymInstances #-}
 module BookBrainz.Forms where
 
-import Control.Applicative ((<$>))
-import Data.Maybe (fromMaybe)
-import Data.Text (Text)
-import Text.Digestive
-import Text.Digestive.Forms.Html
-import qualified Text.Digestive.Forms as Forms
-import Text.Blaze.Html5 (Html, (!), toValue)
-import qualified Text.Blaze.Html5 as H
-import qualified Text.Blaze.Html5.Attributes as A
-import Text.Digestive.Forms.Snap
-import Text.Digestive.Forms (FormInput(..))
-import Text.Digestive.Blaze.Html5 hiding (inputText)
-import Snap.Types
+import           Control.Applicative         ((<$>))
+import           Data.Maybe                  (fromMaybe)
 
-import BookBrainz.Types.Book
+import           Data.Text                   (Text)
+import qualified Data.Text                   as T
+import           Snap.Core
+import           Text.Blaze.Html5            (Html, (!), toValue)
+import qualified Text.Blaze.Html5            as H
+import qualified Text.Blaze.Html5.Attributes as A
+import           Text.Digestive
+import           Text.Digestive.Blaze.Html5  hiding (inputText)
+import           Text.Digestive.Forms        (FormInput(..))
+import qualified Text.Digestive.Forms        as Forms
+import           Text.Digestive.Forms.Snap
+
+import           BookBrainz.Types.Book
 
 data SearchQuery = SearchQuery { query :: Text }
 
 bookForm :: (Monad m, MonadSnap m) => SnapForm m Html BlazeFormHtml Book
 bookForm = Book <$> inputText (Just "")
 
+nonEmpty :: Monad m => Validator m Html Text
+nonEmpty = check "Query cannot be empty" (not . T.null)
+
 searchForm :: (Monad m, MonadSnap m) => SnapForm m Html BlazeFormHtml SearchQuery
-searchForm = SearchQuery <$> inputText (Just "")
+searchForm = SearchQuery <$> inputText (Just "") `validate` nonEmpty
 
 inputText :: (Monad m, Functor m, FormInput i f)
           => Formlet m i e BlazeFormHtml Text
@@ -31,3 +35,9 @@ inputText = Forms.inputText $ \id' inp -> createFormHtml $ \_ ->
   H.input ! A.type_ "text"
           ! A.name (toValue $ show id')
           ! A.value (toValue $ fromMaybe "" inp)
+
+processForm :: (MonadSnap m)
+            => SnapForm m e v a  -- ^ Form
+            -> String            -- ^ Form name
+            -> m (Either v a)    -- ^ Result
+processForm form name = eitherForm form name snapEnvironment
--- src/BookBrainz/Web/View/Search.hs
+++ src/BookBrainz/Web/View/Search.hs
@@ -1,20 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
 module BookBrainz.Web.View.Search where
 
-import           Data.Monoid          (mconcat)
+import           Data.Monoid                 (mconcat)
 
-import           Data.Copointed       (copoint)
-import           Search.ElasticSearch (SearchResults(..)
-                                      ,SearchResult(..))
-import           Text.Blaze           (Html, toHtml)
-import qualified Text.Blaze.Html5 as H
+import           Data.Copointed              (copoint)
+import           Search.ElasticSearch        (SearchResults(..)
+                                             ,SearchResult(..))
+import           Text.Blaze                  (Html, toHtml, (!), toValue)
+import qualified Text.Blaze.Html5            as H
+import qualified Text.Blaze.Html5.Attributes as A
+import           Text.Digestive.Forms.Html   (FormEncType)
 
-import qualified BookBrainz.Search as S
+import qualified BookBrainz.Search           as S
 import           BookBrainz.Types
+import           BookBrainz.Web.Sitemap      as Sitemap (Sitemap(..), showURL)
 import           BookBrainz.Web.View  (pageLayout, detailTable, linkBook
                                       ,linkPerson)
 
-searchResults :: SearchResults S.SearchableBook -> Html
+--------------------------------------------------------------------------------
+-- | Given a list of search results, display them in a human readable
+-- table.
+searchResults :: SearchResults S.SearchableBook
+              -- ^ The search results.
+              -> Html
 searchResults results = pageLayout Nothing $
   detailTable [("Score", ["score"])
               ,("Book", [])
@@ -26,9 +34,21 @@ searchResults results = pageLayout Nothing $
                          , roleList $ S.bookRoles $ result r
                          ]
 
+-- TODO Might be useful elsewhere, so this might end up in View.Role
 roleList :: [(LoadedEntity Role, LoadedCoreEntity Person)]
          -> Html
 roleList roles = H.ul (formatRole `mapM_` roles)
   where formatRole (r, p) = H.li $ do
           linkPerson p
           mconcat [" (", toHtml (roleName (copoint r)), ")"]
+
+--------------------------------------------------------------------------------
+-- | A form for beginning a search
+searchPortal :: (Html, FormEncType)  -- ^ The form 'Html' and the encoding of it.
+             -> Html
+searchPortal (formHtml, enctype) =
+  pageLayout Nothing $ do
+    H.h1 "Search"
+    H.form ! A.method "GET" ! A.action (toValue $ showURL Search) $ do
+      formHtml
+      H.p $ H.input ! A.type_ "submit" ! A.value "Search"
