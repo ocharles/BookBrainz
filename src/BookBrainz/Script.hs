492640898143c70e3ce0f2bd50bf1b6e0f21bedd
Seán de Búrca <leftmostcat@gmail.com>
2011-08-26 19:27:29 -0600

    Fixes for configurator code

--- src/BookBrainz/Script.hs
+++ src/BookBrainz/Script.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 -- | Access the BookBrainz API from a script environment.
 module BookBrainz.Script
@@ -7,7 +8,7 @@ module BookBrainz.Script
        ) where
 
 import Control.Monad.Reader
-import Data.Configurator        (load, lookup, Worth (..))
+import Data.Configurator        (load, require, Worth (..))
 import Database.HDBC.PostgreSQL (Connection)
 
 import BrainzStem.Database      (HasDatabase(..), Database(..), openConnection)
@@ -26,8 +27,8 @@ instance HasDatabase Script where
   askConnection = asks (connectionHandle . modelStateConn)
 
 runScript action = do
-  config <- load [Required "bookbrainz.cfg"]
-  dbName <- require config "database.database"
-  dbUser <- require config "database.user"
+  config <- load [Required "snaplets/database/snaplet.cfg"]
+  dbName <- require config "database"
+  dbUser <- require config "user"
   conn <- openConnection dbName dbUser
   runReaderT (unScript action) $ ScriptState conn
