c610093eaec5fa135b433e05ee50fc5fdb0cec0c
Oliver Charles <oliver.g.charles@googlemail.com>
2011-12-03 16:22:23 +0000

    QuickCheck EditionFormat.getByPk

--- tests/Test/BookBrainz/EditionFormat.hs
+++ tests/Test/BookBrainz/EditionFormat.hs
@@ -2,13 +2,35 @@
 module Test.BookBrainz.EditionFormat (tests) where
 
 import Test.BrainzStem
+import Test.BrainzStem.Gen
+import Test.BrainzStem.Model
+import Test.QuickCheck.Arbitrary (Arbitrary(..))
+import Test.QuickCheck.Gen (Gen, listOf1)
 
+import Control.Applicative
 import Database.HDBC (toSql)
 import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Snap.Snaplet.Hdbc as HDBC
 
+import BrainzStem.Database
 import BookBrainz.Model.EditionFormat
 import BookBrainz.Types
 import BrainzStem.Model
+
+instance Arbitrary EditionFormat where
+  arbitrary = EditionFormat <$> T.pack `fmap` name
+
+instance Arbitrary (InDB EditionFormat LoadedEntity) where
+  arbitrary = do
+    format <- arbitrary
+    return $ InDB format (insertFormat format >>= toEnt format)
+    where insertFormat f =
+            queryOne (unlines ["INSERT INTO edition_format (name) VALUES (?)"
+                              ,"RETURNING id"])
+                     [ toSql $ editionFormatName f ]
+          toEnt r id = return $ Entity { entityRef = Ref id
+                                       , entityInfo = r }
 
 test_editionFormat_allEditionFormats :: DatabaseContext ()
 test_editionFormat_allEditionFormats = do
@@ -18,12 +40,6 @@ test_editionFormat_allEditionFormats = do
     assertBool "Expected to find some formats" (length formats == 2)
     formats !! 0 @?= testFormat 1 "Paperback"
     formats !! 1 @?= testFormat 2 "Hard back"
-
-test_editionFormat_getByPk :: DatabaseContext ()
-test_editionFormat_getByPk = do
-  runRaw initialEnvironment
-  loadedEditionFormat <- getByPk $ Ref (toSql (1 :: Int))
-  liftIO $ loadedEditionFormat @?= testFormat 1 "Paperback"
 
 initialEnvironment :: String
 initialEnvironment = [str|
@@ -37,5 +53,6 @@ testFormat fid name = Entity { entityRef = Ref (toSql fid)
 
 tests :: [Test]
 tests = [ testCase "allEditionFormats" $ databaseTest test_editionFormat_allEditionFormats
-        , testCase "getByPk" $ databaseTest test_editionFormat_getByPk
+        , testProperty "getByPk" $ prop_getByPk
+          ( listOf1 arbitrary :: Gen [InDB EditionFormat LoadedEntity] )
         ]
