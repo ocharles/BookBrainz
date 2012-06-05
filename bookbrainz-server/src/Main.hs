94bcabf8d69954d3b082b649d6d9943717a32770
Oliver Charles <oliver.g.charles@googlemail.com>
2012-06-06 00:18:50 +0100

    bookbrainz-server: Add support for dynamic reloading

--- bookbrainz-server/src/Main.hs
+++ bookbrainz-server/src/Main.hs
@@ -1,7 +1,40 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Main where
+
+import           Control.Exception (SomeException, try)
+import           Snap.Core
+import           Snap.Http.Server
+import           Snap.Snaplet
+import           Snap.Snaplet.Config
+import           System.IO
 import Snap.Http.Server.Config
+import System.Console.GetOpt
 import Snap.Snaplet (serveSnaplet)
+import qualified Data.Text as T
+
+#ifdef DEVELOPMENT
+import           Snap.Loader.Dynamic
+#else
+import           Snap.Loader.Static
+#endif
 
 import BookBrainz.Web (bookbrainz)
 
 main :: IO ()
-main = serveSnaplet emptyConfig bookbrainz
+main = do
+    --serveSnaplet emptyConfig bookbrainz
+    (conf, site, cleanup) <- $(loadSnapTH [| getConf |] 'getActions [])
+    _ <- try $ httpServe conf site :: IO (Either SomeException ())
+    cleanup
+
+getConf :: IO (Config Snap AppConfig)
+getConf = commandLineConfig emptyConfig
+
+getActions :: Config Snap AppConfig -> IO (Snap (), IO ())
+getActions conf = do
+    let env = appEnvironment =<< getOther conf
+    (msgs, site, cleanup) <- runSnaplet env bookbrainz
+    hPutStrLn stderr $ T.unpack msgs
+    return (site, cleanup)
