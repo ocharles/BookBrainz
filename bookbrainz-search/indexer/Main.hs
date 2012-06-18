12f12a43e233b2168de6654f7f8b039f8d1d626e
Oliver Charles <oliver.g.charles@googlemail.com>
2012-06-18 21:15:23 +0100

    bookbrainz-search: Do not use PostgresSQL notifications
    I spoke with the PgQ author and he is not too eager to implement this. I don't
    have any good arguments as to why it's a good idea, so I will trust his
    judgement that 'sleep' is the way to go. I imagine that I will move to RabbitMQ
    before this becomes a problem anyway (as we need it for other time critical
    stuff, like cache invalidation).

--- bookbrainz-search/indexer/Main.hs
+++ bookbrainz-search/indexer/Main.hs
@@ -2,14 +2,14 @@
 module Main where
 
 import Control.Applicative ((<$>), (<*>))
-import Control.Monad (forever, join, void, when)
+import Control.Concurrent (threadDelay)
+import Control.Monad (forever, join, void)
 import Control.Monad.IO.Class (liftIO)
 import Data.Maybe (listToMaybe, fromJust)
 import Data.Traversable (traverse)
 import Database.PostgreSQL.Simple (Only(..))
 import Database.PostgreSQL.Simple.FromRow (FromRow(..), field)
-import Database.PostgreSQL.Simple.Notification (getNotification, notificationChannel)
-import Snap.Snaplet.PostgresqlSimple (query, withPG, execute_)
+import Snap.Snaplet.PostgresqlSimple (query)
 
 import BrainzStem.Model (getByBbid)
 import BookBrainz.Script
@@ -22,7 +22,7 @@ instance FromRow Event where
   fromRow = Event <$> field <*> fmap (fromJust . parseBbid) field
 
 dispatchEvent :: Event -> Script ()
-dispatchEvent ev = do
+dispatchEvent ev = void $ do
   getByBbid (eventBbid ev) >>= traverse indexBook
   liftIO $ putStrLn $ "Indexed " ++ show (eventBbid ev)
 
@@ -39,21 +39,10 @@ consumeBatches = do
     Just b -> runBatch b >> consumeBatches
     Nothing -> return ()
 
-indexer :: Script ()
-indexer = do
-  liftIO $ putStrLn "Consuming existing batches"
-  consumeBatches
-
-  liftIO $ putStrLn "Waiting for index events"
-  execute_ "LISTEN pgqticker"
-  forever $ do
-    n <- withPG $ \c -> getNotification c
-    when (notificationChannel n == "pgqticker") consumeBatches
-
 nextBatchId :: Script (Maybe Int)
 nextBatchId =
   (join . fmap fromOnly . listToMaybe) <$> query "SELECT pgq.next_batch(?, ?)"
     ("foo" :: String, "bar" :: String)
 
 main :: IO ()
-main = runScript indexer
+main = runScript $ void $ forever (consumeBatches >> liftIO (threadDelay (5 * 1000)))
