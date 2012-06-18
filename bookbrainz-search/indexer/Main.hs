138a4e7c5db8580b437c1d6ff67190a75bdc07f3
Oliver Charles <oliver.g.charles@googlemail.com>
2012-06-18 23:28:33 +0100

    bookbrainz-search: Use RabbitMQ over PgQ

--- bookbrainz-search/indexer/Main.hs
+++ bookbrainz-search/indexer/Main.hs
@@ -1,49 +1,35 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Control.Applicative ((<$>), (<*>))
-import Control.Concurrent (threadDelay)
 import Control.Monad (forever, void)
 import Control.Monad.IO.Class (liftIO)
+import qualified Data.ByteString.Lazy.Char8 as LBS
 import Data.Maybe (fromJust)
 import Data.Traversable (traverse)
-import Database.PostgreSQL.Simple (Only(..))
-import Database.PostgreSQL.Simple.FromRow (FromRow(..), field)
-import Snap.Snaplet.PostgresqlSimple (query)
+import Network.AMQP
 
 import BrainzStem.Model (getByBbid)
 import BookBrainz.Script
 import BookBrainz.Search (indexBook)
 import BookBrainz.Types
 
-data Event = Event { eventId :: Int, eventBbid :: BBID Book }
-
-instance FromRow Event where
-  fromRow = Event <$> field <*> fmap (fromJust . parseBbid) field
-
-dispatchEvent :: Event -> Script ()
-dispatchEvent ev = void $ do
-  getByBbid (eventBbid ev) >>= traverse indexBook
-  liftIO $ putStrLn $ "Indexed " ++ show (eventBbid ev)
-
-runBatch :: Int -> Script ()
-runBatch batchId = do
-  query "SELECT ev_id, ev_data FROM pgq.get_batch_events(?)" (Only batchId) >>=
-    mapM_ dispatchEvent
-  void $ (query "SELECT pgq.finish_batch(?)" (Only batchId) :: Script [Only Int])
-
-consumeBatches :: Script ()
-consumeBatches = do
-  batchId <- nextBatchId
-  case batchId of
-    Just b -> runBatch b >> consumeBatches
-    Nothing -> return ()
-
-nextBatchId :: Script (Maybe Int)
-nextBatchId =
-  (fromOnly . head) <$> query "SELECT pgq.next_batch(?, ?)"
-    ("foo" :: String, "bar" :: String)
+handleIndex :: (Message, Envelope) -> Script ()
+handleIndex (msg, _) = do
+  getByBbid bbid' >>= traverse indexBook
+  liftIO $ putStrLn $ "Indexed " ++ show bbid'
+  where bbid' = (fromJust . parseBbid . LBS.unpack . msgBody $ msg)
 
 main :: IO ()
-main = runScript $ void $ forever $
-  consumeBatches >> liftIO (threadDelay (5 * 1000 * 1000))
+main = runScript $ void $ liftIO $ do
+  conn <- openConnection "127.0.0.1" "/" "guest" "guest"
+  chan <- openChannel conn
+
+  declareQueue chan newQueue {queueName = "database"}
+  declareExchange chan newExchange
+      { exchangeName = "database"
+      , exchangeType = "fanout"
+      }
+  bindQueue chan "" "database" "database"
+
+  consumeMsgs chan "database" Ack (runScript . handleIndex)
+  forever getLine
