b6ceea90e522014b126edd78f5024e73256c642e
Oliver Charles <oliver.g.charles@googlemail.com>
2012-06-23 23:27:13 +0100

    bookbrainz-search: Change the reindex script to just message the search queue
    Instead of actually doing work, this script just pushes all known BBIDs into the
    message queue, and lets the daemon deal with it.

--- bookbrainz-search/reindex/Main.hs
+++ bookbrainz-search/reindex/Main.hs
@@ -1,18 +1,33 @@
 module Main where
 
+import qualified Data.ByteString.Lazy.Char8 as LBS
+import           Control.Monad.IO.Class (liftIO)
+import           Network.AMQP
+
 import           BookBrainz.Model.Book
 import           BookBrainz.Model.Edition
 import           BookBrainz.Model.Person
 import           BookBrainz.Model.Publisher
 import           BookBrainz.Script
-import           BookBrainz.Search
-
-reindexEverything :: Script ()
-reindexEverything = do
-  listAllBooks >>= mapM_ indexBook
-  listAllEditions >>= mapM_ indexEdition
-  allPersons >>= mapM_ indexPerson
-  allPublishers >>= mapM_ indexPublisher
+import           BookBrainz.Types
 
 main :: IO ()
-main = runScript reindexEverything
+main = runScript $ do
+  chan <- liftIO $
+    openConnection "127.0.0.1" "/" "guest" "guest" >>= openChannel
+
+  mapM_ (dispatch chan) types
+
+  where
+    types = [ (bbids listAllBooks ,"book")
+            , (bbids allEditions, "edition")
+            , (bbids allPersons, "person")
+            , (bbids allPublishers, "publisher")
+            ]
+    publish chan t bbid' = publishMsg chan "search" t
+      newMsg { msgBody = LBS.pack bbid'
+             , msgDeliveryMode = Just Persistent
+             }
+    dispatch chan (lister, queue) =
+      lister >>= liftIO . mapM_ (publish chan queue)
+    bbids = fmap (map (show . bbid))
